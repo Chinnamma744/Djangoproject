82a00f37458a289496b908bf7c8d8a5eb19ea980
Chinnamma744 <chinnusf1234@gmail.com>
2024-12-20 10:46:55 +0530

    this is my project

---
+++ myproject/myapp/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class student(models.Model):
+    name=models.TextField(max_length=20)
+    age=models.IntegerField()
+
+    def __str__(self):
+        return self.name + " " + str(self.age)
+    
+class Game(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField(max_length=100)
+
+    def __str__(self):
+        return self.name + " " + self.description
